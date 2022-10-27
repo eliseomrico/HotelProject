9355265b4fcd229c8dd0086532c306e27bf17837
AshleyALand <77741512+AshleyALand@users.noreply.github.com>
2022-10-27 18:28:01 -0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,42 @@
+namespace HotelMS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void spacerPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void logOut_Click(object sender, EventArgs e)
+        {
+            EmployeeLogin employeeLogin = new HotelMS.EmployeeLogin();
+            employeeLogin.Show();
+
+        }
+    }
+}
