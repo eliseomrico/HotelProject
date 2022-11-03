e7ea78eaa787a427a07a28c570608aecd7a9e066
Matt Olivarez <mjolivarez1994@gmail.com>
2022-11-03 11:55:12 -0500

    added room availability form

---
+++ HotelApp_v1/RoomAvailabilityForm.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace HotelApp_v1
+{
+    partial class RoomAvailabilityForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RoomAvailabilityForm));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button_search = new System.Windows.Forms.Button();
+            this.button_create = new System.Windows.Forms.Button();
+            this.button_delete = new System.Windows.Forms.Button();
+            this.button_edit = new System.Windows.Forms.Button();
+            this.button_finish_edit = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox_res_id = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(78)))), ((int)(((byte)(79)))));
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.button_search);
+            this.panel1.Controls.Add(this.button_create);
+            this.panel1.Controls.Add(this.button_delete);
+            this.panel1.Controls.Add(this.button_edit);
+            this.panel1.Controls.Add(this.button_finish_edit);
+            this.panel1.Location = new System.Drawing.Point(-3, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(217, 548);
+            this.panel1.TabIndex = 21;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(215, 169);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 22;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button_search
+            // 
+            this.button_search.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(78)))), ((int)(((byte)(79)))));
+            this.button_search.FlatAppearance.BorderSize = 0;
+            this.button_search.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_search.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_search.ForeColor = System.Drawing.Color.White;
+            this.button_search.Location = new System.Drawing.Point(3, 470);
+            this.button_search.Name = "button_search";
+            this.button_search.Size = new System.Drawing.Size(214, 82);
+            this.button_search.TabIndex = 15;
+            this.button_search.Text = "Search";
+            this.button_search.UseVisualStyleBackColor = false;
+            // 
+            // button_create
+            // 
+            this.button_create.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(78)))), ((int)(((byte)(79)))));
+            this.button_create.FlatAppearance.BorderSize = 0;
+            this.button_create.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_create.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_create.ForeColor = System.Drawing.Color.White;
+            this.button_create.Location = new System.Drawing.Point(3, 224);
+            this.button_create.Name = "button_create";
+            this.button_create.Size = new System.Drawing.Size(214, 82);
+            this.button_create.TabIndex = 19;
+            this.button_create.Text = "Create";
+            this.button_create.UseVisualStyleBackColor = false;
+            // 
+            // button_delete
+            // 
+            this.button_delete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(78)))), ((int)(((byte)(79)))));
+            this.button_delete.FlatAppearance.BorderSize = 0;
+            this.button_delete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_delete.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_delete.ForeColor = System.Drawing.Color.White;
+            this.button_delete.Location = new System.Drawing.Point(3, 306);
+            this.button_delete.Name = "button_delete";
+            this.button_delete.Size = new System.Drawing.Size(214, 82);
+            this.button_delete.TabIndex = 16;
+            this.button_delete.Text = "Delete";
+            this.button_delete.UseVisualStyleBackColor = false;
+            this.button_delete.Visible = false;
+            // 
+            // button_edit
+            // 
+            this.button_edit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(78)))), ((int)(((byte)(79)))));
+            this.button_edit.FlatAppearance.BorderSize = 0;
+            this.button_edit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_edit.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_edit.ForeColor = System.Drawing.Color.White;
+            this.button_edit.Location = new System.Drawing.Point(3, 388);
+            this.button_edit.Name = "button_edit";
+            this.button_edit.Size = new System.Drawing.Size(214, 82);
+            this.button_edit.TabIndex = 17;
+            this.button_edit.Text = "Edit";
+            this.button_edit.UseVisualStyleBackColor = false;
+            this.button_edit.Visible = false;
+            // 
+            // button_finish_edit
+            // 
+            this.button_finish_edit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(78)))), ((int)(((byte)(79)))));
+            this.button_finish_edit.FlatAppearance.BorderSize = 0;
+            this.button_finish_edit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_finish_edit.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_finish_edit.ForeColor = System.Drawing.Color.White;
+            this.button_finish_edit.Location = new System.Drawing.Point(3, 388);
+            this.button_finish_edit.Name = "button_finish_edit";
+            this.button_finish_edit.Size = new System.Drawing.Size(214, 82);
+            this.button_finish_edit.TabIndex = 18;
+            this.button_finish_edit.Text = "Save Edit";
+            this.button_finish_edit.UseVisualStyleBackColor = false;
+            this.button_finish_edit.Visible = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.dataGridView1);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.textBox_res_id);
+            this.panel2.Location = new System.Drawing.Point(208, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(582, 548);
+            this.panel2.TabIndex = 22;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(165, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Location Name";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.White;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(105, 30);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(389, 28);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Enter Reservation ID and click Search below";
+            // 
+            // textBox_res_id
+            // 
+            this.textBox_res_id.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_res_id.Location = new System.Drawing.Point(165, 120);
+            this.textBox_res_id.Name = "textBox_res_id";
+            this.textBox_res_id.Size = new System.Drawing.Size(227, 27);
+            this.textBox_res_id.TabIndex = 7;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 176);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(579, 372);
+            this.dataGridView1.TabIndex = 15;
+            // 
+            // RoomAvailabilityForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(786, 548);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "RoomAvailabilityForm";
+            this.Text = "RoomAvailabilityForm";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button_search;
+        private System.Windows.Forms.Button button_create;
+        private System.Windows.Forms.Button button_delete;
+        private System.Windows.Forms.Button button_edit;
+        private System.Windows.Forms.Button button_finish_edit;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox_res_id;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
